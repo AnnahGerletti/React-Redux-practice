80fcd1b5441a57c41bbccdc87752bad8e5b19816
AnnahGerletti <agerletti@gmail.com>
2017-12-12 18:26:47 +1300

    setting up first instance of app rendering in broweser

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 // create a new component
 // produce some html
-//Take the compt and generated html and put it on the page. 
+//Take the compt and generated html and put it on the page.
+
+const App = () => {
+  return <div>hi</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))

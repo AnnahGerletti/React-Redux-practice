b6db623f1207d295c6f9866de5aed585d6958971
AnnahGerletti <agerletti@gmail.com>
2017-12-12 21:39:42 +1300

    reviewing state

--- src/component/Search_bar.jsx
+++ src/component/Search_bar.jsx
@@ -9,14 +9,11 @@ class SearchBar extends Component {
 render (){
     return (
     <div className = 'searchBar'>
-     <input onChange ={this.onInputChange}/>
-     <input type = "submit" />
+     <input
+       value = {this.state.term}
+       onChange ={event => this.setState({term:event.target.value})}/>
     </div>
-  )}
-
-  onInputChange(event){
-    console.log(event.target.value)
-  }
+  )
 }
-
+}
 export default SearchBar

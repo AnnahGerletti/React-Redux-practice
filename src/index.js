5afa7d5e5d15ccb8f2b27db33e346f3bda03b21c
AnnahGerletti <agerletti@gmail.com>
2017-12-15 12:27:27 +1300

    changed App to class base Downward data flow

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import YTsearch from 'youtube-api-search'
 
 import SearchBar from "./component/Search_bar"
 import Header from "./component/Header"
 
 const API_key = "AIzaSyAm9Eq9NO69VKGqSU02R6fSb9YDCKkWv0M"
-// create a new component
-// produce some html
-//Take the compt and generated html and put it on the page.
 
-const App = () => {
+YTsearch({key: API_key, term: "cats" }, function(data){
+  console.log(data)
+})
+
+
+class App extends Component {
+  render (){
   return (
     <div>
       <Header />
       <SearchBar />
     </div>
   )}
+  }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
